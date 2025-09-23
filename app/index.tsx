c0713ec6eb14c201c70a222a4ecfd7a6e159886b
Angelhpascual <Angelhpascual@gmail.com>
2025-09-23 12:17:27 +0200

    fix: update RootLayout to hide header and enhance Index layout

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native"
 
 export default function Index() {
   return (
-    <View className="bg-pink-400">
+    <View className="bg-pink-400 flex-1 items-center justify-center h-screen">
       <Text className="text-blue-500 font-bold">
         Edit app/index.tsx to edit this screen.
       </Text>
